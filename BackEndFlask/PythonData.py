ab697bab3cd86d53a1392a7ae2644c9657bfc57a
Sujit Das <63442342+SujitDas-tech@users.noreply.github.com>
2022-05-28 15:48:40 +0530

    Update PythonData.py

--- BackEndFlask/PythonData.py
+++ BackEndFlask/PythonData.py
@@ -1,10 +1,10 @@
 import pymssql
 def DownloadPath(BoilerID):
     Path=""
-    server="sql8003.site4now.net"
-    user="db_a8793c_boilerdb_admin"
-    password="Sujit123"
-    myconn = pymssql.connect(server,user,password,"db_a8793c_boilerdb")
+    server="localhost"
+    user=""
+    password=""
+    myconn = pymssql.connect(server,user,password,"DB")
     cursor = myconn.cursor()
     sql = "SELECT * FROM TblBoilerLookUp WHERE BoilerID=%s"
     cursor.execute(sql,BoilerID)
